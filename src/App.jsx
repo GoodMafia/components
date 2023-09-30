065106f1fefcc98c568737fce44415cbdeba073a
GoodMafia <danya.makagonov@internet.ru>
2023-09-30 17:55:46 +0500

    components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Footer/>
+      <Header/>
+      <Main/>
+
+    </div>
+  );
+}
+
+export default App;
